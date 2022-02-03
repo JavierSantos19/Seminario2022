022b9a246de63f2a9131fe5005c3a7a46a31761d
JavierSantos19 <javiereduardo_santoschevez@yahoo.com>
2022-02-02 19:33:46 -0600

    API

---
+++ __test__/dao/pacientes.model.test.js
@@ -0,0 +1,16 @@
+/*const Pacientes = require('../../dao/pacientes/pacientes.model');
+describe('Testing Pacientes Model', () => {
+    let pacientesmodel = null;
+    beforeAll(() => {
+        pacientesmodel = new Pacientes();
+    })
+
+    it('pacientesModel Esta Definido', () => {
+        expect(pacientesmodel).toBeDefined();
+    })
+
+    it('getAll Devuelve un array', async() => {
+        const arrPacientes = await pacientesmodel.getAll();
+        return expect(arrPacientes.length).toBeGreaterThanOrEqual(0);
+    })
+})*/
